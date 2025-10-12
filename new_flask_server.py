491d032b41810f501a559375c2acf8f2af72cb4f
jbaremoney <114310093+jbaremoney@users.noreply.github.com>
2025-10-11 20:15:41 -0500

    changing port

--- new_flask_server.py
+++ new_flask_server.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from server_utils import *
-from models import BlowState, BlowSession
+from models import BlowSession
 from threading import Lock
 import pandas as pd
 
@@ -146,4 +146,4 @@ def get_most_recent():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=8000)
